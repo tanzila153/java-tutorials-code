af6416cdec922cbfe3898a650a833ee1355e7838
Tanjila Akter <114749304+tanzila153@users.noreply.github.com>
2022-11-16 20:21:53 +0600

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -11,3 +11,38 @@ price: 1895 euros,
 description: perfect product with best image quality,
 category: phone,
  */
+
+
+package assaignment1;
+
+
+class Product {
+
+     int id;
+     String title;
+     String price;
+     String description;
+     String catagory;
+     
+    public static void main(String[] args) {
+      
+        Product product1 = new Product();
+        product1.id = 101;
+        product1.price = "1895 euros";
+       product1.title = "iphone15" ;
+       product1.description ="perfect product with best image quality" ;
+        product1.catagory ="Phone" ;
+       
+        System.out.println("id =" + product1.id);
+          System.out.println("title =" + product1.title);
+          System.out.println("price =" + product1.price);
+          System.out.println("description =" + product1.description);
+              System.out.println("catagory =" + product1.catagory);
+                
+                
+    }
+    
+} 
+
+
+
